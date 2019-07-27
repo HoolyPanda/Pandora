fa053a37ed42e06803db1af971c1992c3722b672
HoolyPanda <pavlov.ss10@gmail.com>
2019-07-27 04:25:42 +0300

    Beta

---
+++ Esp8266/src/main.cpp
@@ -0,0 +1,143 @@
+
+/*
+ * ----------------------------------------------------------------------
+ * Example program showing how to read new NUID from a PICC to serial.
+ * ----------------------------------------------------------------------
+ * https://circuits4you.com
+ * 
+ * RC522 Interfacing with NodeMCU
+ * 
+ * Typical pin layout used:
+ * ----------------------------------
+ *             MFRC522      Node     
+ *             Reader/PCD   MCU      
+ * Signal      Pin          Pin      
+ * ----------------------------------
+ * RST/Reset   RST          D1 (GPIO5)        
+ * SPI SS      SDA(SS)      D2 (GPIO4)       
+ * SPI MOSI    MOSI         D7 (GPIO13)
+ * SPI MISO    MISO         D6 (GPIO12)
+ * SPI SCK     SCK          D5 (GPIO14)
+ * 3.3V        3.3V         3.3V
+ * GND         GND          GND
+ */
+ 
+#include <SPI.h>
+#include <MFRC522.h>
+#include <Servo.h>
+
+#define ServoPin 14
+
+constexpr uint8_t RST_PIN = 5;     // Configurable, see typical pin layout above
+constexpr uint8_t SS_PIN = 4;     // Configurable, see typical pin layout above
+ 
+MFRC522 rfid(SS_PIN, RST_PIN); // Instance of the class
+
+MFRC522::MIFARE_Key key; 
+ 
+Servo myservo;
+// Init array that will store new NUID 
+byte nuidPICC[4];
+void printHex(byte *buffer, byte bufferSize) {
+  for (byte i = 0; i < bufferSize; i++) {
+    Serial.print(buffer[i] < 0x10 ? " 0" : " ");
+    Serial.print(buffer[i], HEX);
+  }
+}
+ 
+/**
+ * Helper routine to dump a byte array as dec values to Serial.
+ */
+void printDec(byte *buffer, byte bufferSize) {
+  for (byte i = 0; i < bufferSize; i++) {
+    Serial.print(buffer[i] < 0x10 ? " 0" : " ");
+    Serial.print(buffer[i], DEC);
+  }
+}
+void setup() { 
+    Serial.begin(9600);
+    SPI.begin(); // Init SPI bus
+    rfid.PCD_Init(); // Init MFRC522 
+    // Serial.println("Input position");
+    // String sPos = Serial.readString();
+    // int pos = sPos.toInt();
+    // myservo.write(0);
+    Serial.println("2");
+    Serial.println("1");
+  for (byte i = 0; i < 6; i++) {
+    key.keyByte[i] = 0xFF;
+  }
+ 
+  // myservo.attach(D5);
+  delay(50);
+  // myservo.write(90);
+  delay(2000);
+  myservo.write(0);
+
+  Serial.println(F("This code scan the MIFARE Classsic NUID."));
+  myservo.attach(D4);
+  Serial.print(F("Using the following key:"));
+  printHex(key.keyByte, MFRC522::MF_KEY_SIZE);
+}
+ int servoPos = 1;
+void loop() {
+  // Look for new cards
+  if ( ! rfid.PICC_IsNewCardPresent())
+    return;
+ 
+  // Verify if the NUID has been readed
+  if ( ! rfid.PICC_ReadCardSerial())
+    return;
+ 
+  if (rfid.uid.uidByte[0] != nuidPICC[0] || 
+    rfid.uid.uidByte[1] != nuidPICC[1] || 
+    rfid.uid.uidByte[2] != nuidPICC[2] || 
+    rfid.uid.uidByte[3] != nuidPICC[3] ) {
+    Serial.println(F("A new card has been detected."));
+ 
+    // Store NUID into nuidPICC array
+    for (byte i = 0; i < 4; i++) {
+      nuidPICC[i] = rfid.uid.uidByte[i];
+    }
+   
+    // Serial.println(F("The NUID tag is:"));
+    Serial.println(int(rfid.uid.uidByte));
+    // printHex(rfid.uid.uidByte, rfid.uid.size);
+    Serial.println();
+    // Serial.print(F("In dec: "));
+    // printDec(rfid.uid.uidByte, rfid.uid.size);
+    Serial.println();
+  }
+  else 
+  {
+    Serial.println(F("Card read previously."));
+    Serial.println(int(rfid.uid.uidByte)); 
+    if (myservo.attached())
+    {
+      Serial.println("Servo online");
+      if (servoPos != 120)
+      {
+        servoPos = 120;
+      }
+      else
+      {
+        servoPos = 1;
+      }
+
+      myservo.write(servoPos);
+      Serial.println(myservo.read());
+      // myservo.write(Serial.readStringUntil('\n').toInt());
+    }
+  }
+ 
+  // Halt PICC
+  rfid.PICC_HaltA();
+ 
+  // Stop encryption on PCD
+  rfid.PCD_StopCrypto1();
+}
+ 
+ 
+/**
+ * Helper routine to dump a byte array as hex values to Serial. 
+ */
---
+++ ProMini/src/main.cpp
@@ -0,0 +1,36 @@
+#include <Arduino.h>
+#include <Servo.h>
+#include <SPI.h>
+#include <MFRC522.h>
+
+#define RST_PIN         9          // Configurable, see typical pin layout above
+#define SS_PIN          10         // Configurable, see typical pin layout above
+
+MFRC522 mfrc522(SS_PIN, RST_PIN);  // Create MFRC522 instance
+Servo myservo;
+
+void setup() {
+
+  myservo.attach(10);
+	Serial.begin(9600);		// Initialize serial communications with the PC
+	while (!Serial);		// Do nothing if no serial port is opened (added for Arduinos based on ATMEGA32U4)
+	SPI.begin();			// Init SPI bus
+	mfrc522.PCD_Init();		// Init MFRC522
+	mfrc522.PCD_DumpVersionToSerial();	// Show details of PCD - MFRC522 Card Reader details
+	Serial.println(F("Scan PICC to see UID, SAK, type, and data blocks..."));
+}
+
+void loop() {
+	// Reset the loop if no new card present on the sensor/reader. This saves the entire process when idle.
+	if ( ! mfrc522.PICC_IsNewCardPresent()) {
+		return;
+	}
+
+	// Select one of the cards
+	if ( ! mfrc522.PICC_ReadCardSerial()) {
+		return;
+	}
+  // mfrc522.
+	// Dump debug info about the card; PICC_HaltA() is automatically called
+	mfrc522.PICC_DumpToSerial(&(mfrc522.uid));
+}
